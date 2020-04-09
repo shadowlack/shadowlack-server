0e8d24e42457da273870d679e67e10b9064a3d4c
Jodie Struthers <jodie@shriker.ca>
2020-04-09 15:16:24 -0600

    Working on main navigation

--- shadowlack/web/urls.py
+++ shadowlack/web/urls.py
@@ -8,11 +8,13 @@ from django.urls import path, include
 
 # default evennia patterns
 from evennia.web.urls import urlpatterns
+import paxboards.urls
 
 # eventual custom patterns
 custom_patterns = [
     # url(r'/desired/url/', view, name='example'),
     path("", include("web.character.urls")),
+    path("forums/", include('paxboards.urls', namespace='board')),
 ]
 
 # this is required by Django.
